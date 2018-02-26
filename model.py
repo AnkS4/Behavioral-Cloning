34066b615a12018fa801af709e2195a6d063b432
Aniket Satbhai <AnkS4@users.noreply.github.com>
2018-02-26 23:50:09 +0530

    switched to fit_generator

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D, Cropping2D
 from keras import backend as K
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 lines = []
 with open('./data/driving_log.csv') as f:
@@ -12,70 +14,38 @@ with open('./data/driving_log.csv') as f:
 	for line in reader:
 		lines.append(line)
 
-images = []
-measurements = []
+train_samples, validation_samples = train_test_split(lines, test_size=0.33)
+
 correction = 0.2
-for line in lines:
-	for i in range(3):
-		source_path = line[i]
-		filename = source_path.split('/')[-1]
-		local_path = './data/IMG/' + filename
-		image = cv2.imread(local_path)
-		#image = cv2.resize(image, (32, 32))
-		#image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-		images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
-	measurements.append(measurement+correction)
-	measurements.append(measurement-correction)
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-print(len(images))
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+            	for i in range(3):
+            		path = './data/IMG/' + batch_sample[i].split('/')[-1]
+            		image = cv2.imread(path)
+            		images.append(image)
+            		images.append(cv2.flip(image, 1))
+            	angle = float(batch_sample[3])
+            	angles.append(angle)
+            	angles.append(angle+correction)
+            	angles.append(angle-correction)
+            	angles.append(-1.0 * angle)
+            	angles.append(-1.0 * (angle+correction))
+            	angles.append(-1.0 * (angle-correction))
 
-lines2 = []
-with open('./Car_Train2/driving_log.csv') as f:
-	reader = csv.reader(f)
-	for line in reader:
-		lines2.append(line)
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield shuffle(X_train, y_train)
 
-for line in lines2:
-	for i in range(3):
-		source_path = line[i]
-		filename = source_path.split('/')[-1]
-		local_path = './Car_Train2/IMG/' + filename
-		image = cv2.imread(local_path)
-		images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
-	measurements.append(measurement+correction)
-	measurements.append(measurement-correction)
-
-print(len(images))
-
-'''
-conv = np.array([[0.2989],[0.5870],[0.1140]])
-def grayconv(img):
-	img2 = np.zeros((img.shape[0],img.shape[1],1))
-	img2[:,:,:,] = np.dot(img[...,:3],conv)
-	return img2
-	return (0.21 * img[:,:,:1]) + (0.72 * img[:,:,1:2]) + (0.07 * img[:,:,-1:])
-'''
-
-'''
-augmented_images, augmented_measurements= [], []
-for image, measurement in zip(images, measurements):
-	augmented_images.append(image)
-	augmented_images.append(cv2.flip(image, 1))
-	augmented_measurements.append(measurement)
-	augmented_measurements.append(-1.0 * measurement)
-
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
-'''
-
-X_train = np.array(images)
-y_train = np.array(measurements)
-
-print(X_train.shape)
+train_generator = generator(train_samples)
+validation_generator = generator(validation_samples)
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
@@ -91,22 +61,7 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
-'''
-model = Sequential()
-model.add(Lambda(lambda x: grayconv(x), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: x/255.0 - 0.5))
-model.add(Cropping2D(cropping=((50, 20), (0, 0))))
-
-model.add(Lambda(lambda image:K.tf.image.resize_images(image, size=(32, 32))))
-model.add(Conv2D(32, (3, 3), strides=(2, 2), activation='elu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Conv2D(12, (3, 3), strides=(2, 2), activation='elu'))
-model.add(MaxPooling2D((2,2)))
-model.add(Flatten())
-model.add(Dense(30))
-model.add(Dense(5))
-model.add(Dense(1))
-'''
 model.compile(optimizer='adam', loss='mse')
-history_object = model.fit(X_train, y_train, validation_split=0.33, shuffle=True, epochs=5)
+#model.fit(X_train, y_train, validation_split=0.33, shuffle=True, epochs=5)
+model.fit_generator(train_generator, steps_per_epoch= len(train_samples), validation_data=validation_generator, validation_steps=len(validation_samples), epochs=5, verbose = 1)
 model.save('model.h5')
