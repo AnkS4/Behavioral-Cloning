362487672c4569735367e2a67319ddbaf782fd4f
Aniket Satbhai <AnkS4@users.noreply.github.com>
2018-03-14 14:35:50 +0530

    Changed image channel to 1

--- drive.py
+++ drive.py
@@ -16,6 +16,8 @@ from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
 
+import cv2
+
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
@@ -61,7 +63,11 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+
+        image = cv2.cvtColor(image_array, cv2.COLOR_BGR2HSV)
+        tmp = image[:,:,1].reshape(160, 320, 1)
+
+        steering_angle = float(model.predict(tmp[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
 
